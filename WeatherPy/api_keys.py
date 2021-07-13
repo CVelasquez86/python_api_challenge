fd69f3e17af12dda214a8428b19a5b895eac60de
CVelasquez86 <Cvelasquezc8607@yahoo.com>
2021-07-13 09:35:35 -0700

    Added API Keys.

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "af63661253f7cf253a9f7e89034bfaa3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCx9O1voObxNh2eFJqsC3cwd41cA6mONoc"
